26be325a8df37487f71a0e6d8fe37201e9e54004
Chiara Stefanelli <chiarastefanelli27@gmail.com>
2022-12-04 23:09:48 +0200

    Added sorting for search articles

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ClipLoader from "react-spinners/ClipLoader";
 import { useParams } from "react-router-dom";
 
@@ -11,9 +11,45 @@ function SearchResults() {
     useGlobalContext();
   const { query } = useParams();
 
-  useEffect(() => {
+  React.useEffect(() => {
     searchArticles(query);
   }, [query, searchArticles]);
+
+  // Sort articles
+  // Sort by reference, newest or oldest
+  const [sortType, setSortType] = React.useState("");
+  const [sortedArray, setSortedArray] = React.useState([]);
+
+  React.useEffect(() => {
+    setSortedArray(searchedArticles);
+  }, [searchedArticles]);
+
+  React.useEffect(() => {
+    // Articles arrays sorted by newest and oldest
+    const newestArray = [...searchedArticles].sort(function (a, b) {
+      return new Date(b.pub_date) - new Date(a.pub_date);
+    });
+    const oldestArray = [...searchedArticles].sort(function (a, b) {
+      return new Date(a.pub_date) - new Date(b.pub_date);
+    });
+
+    // Sort array based on sort type
+    if (sortType) {
+      switch (sortType) {
+        case "revelance":
+          setSortedArray(searchedArticles);
+          break;
+        case "newest":
+          setSortedArray(newestArray);
+          break;
+        case "oldest":
+          setSortedArray(oldestArray);
+          break;
+        default:
+          setSortedArray(searchedArticles);
+      }
+    }
+  }, [sortType, searchedArticles]);
 
   // Check if there are search results
   if (loaded && searchedArticles.length < 1) {
@@ -29,12 +65,24 @@ function SearchResults() {
 
   return (
     <div className={style.container}>
-      <span className={style.preTitle}>Showing results for:</span>
-      <h2 className={style.title}>{formatSection(query)}</h2>
+      <div className={style.titleElement}>
+        <div>
+          <span className={style.preTitle}>Showing results for:</span>
+          <h2 className={style.title}>{formatSection(query)}</h2>
+        </div>
+        <select
+          className={style.sortOptions}
+          onChange={(e) => setSortType(e.target.value)}
+        >
+          <option value="revelance">Sort by Revelance</option>
+          <option value="newest">Sort by Newest</option>
+          <option value="oldest">Sort by Oldest</option>
+        </select>
+      </div>
       <hr />
       {loaded ? (
         <div className={style.container}>
-          {searchedArticles.map((article, index) => {
+          {sortedArray.map((article, index) => {
             return <SearchArticle key={index} {...article} />;
           })}
         </div>
